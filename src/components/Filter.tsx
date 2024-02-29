66c2d38762351dff09e8013c33e29b4bd8321072
mennaafathy02 <mennaafathy1014@gmail.com>
2024-02-29 12:48:14 +0200

    responsive homepage

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,5 +1,22 @@
+import { Button } from "./ui/button";
+
 const Filter = () => {
-  return <div>Filter</div>;
+  return (
+    <div className="flex gap-2 flex-wrap justify-center">
+      <Button className="bg-blue-100 px-4 md:px-10 border-blue-800 border text-blue-950 hover:bg-blue-300">
+        All
+      </Button>
+      <Button className="bg-blue-100 px-4 md:px-10 border-blue-800 border text-blue-950 hover:bg-blue-300">
+        Mock interview
+      </Button>
+      <Button className="bg-blue-100 px-4 md:px-10 border-blue-800 border text-blue-950 hover:bg-blue-300">
+        Consultation
+      </Button>
+      <Button className="bg-blue-100 px-4 md:px-10 border-blue-800 border text-blue-950 hover:bg-blue-300">
+        Mentoring
+      </Button>
+    </div>
+  );
 };
 
 export default Filter;
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,13 @@
+import { Input } from "./ui/input";
+import { FaSearch } from "react-icons/fa";
+
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="relative  py-3 mx-auto md:w-96">
+      <Input className="rounded-full" />
+      <FaSearch className="absolute right-3 top-6" />
+    </div>
+  );
 };
 
 export default Search;
