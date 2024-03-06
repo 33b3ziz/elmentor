120e763a3c7e143cd1e91b5b46d70c729924b97f
mennaafathy02 <mennaafathy1014@gmail.com>
2024-03-06 11:32:12 +0200

    mentor page and some changes

---
+++ src/components/MentorDescription.tsx
@@ -0,0 +1,28 @@
+import { Button } from "./ui/button";
+
+const MentorDescription = () => {
+  return (
+    <div className="py-4">
+      <div>
+        <h2 className="font-bold text-lg">Description about me</h2>
+        <p>
+          I've been working with Students remotely for seven years . My
+          experience has likely allowed you to build strong communication
+          skills, enabling me to connect with students , understand their needs,
+          and deliver effective and inspiring sessions. My goal is support
+          stutends in achieving their educational and professional objectives .
+        </p>
+      </div>
+      <div className="my-6">
+        <Button className="mr-5 text-slate-600 bg-white border-2 border-slate-600 hover:text-white">
+          Message
+        </Button>
+        <Button className="mr-5 text-slate-600 bg-white border-2 border-slate-600 hover:text-white">
+          Book a session
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default MentorDescription;
