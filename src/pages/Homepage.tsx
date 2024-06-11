d9dd0f2c9b038cc6dec2156571b5c35608c32fbb
Hossam26 <hassonahossam@mint-ops.com>
2024-06-11 17:10:05 +0300

    filter

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,6 +1,6 @@
-import Filter from "@/components/Filter";
 import MentorList from "@/components/MentorList";
 import Search from "@/components/Search";
+import { Button } from "@/components/ui/button";
 import {
   Pagination,
   PaginationContent,
@@ -10,22 +10,73 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
-import { getMentorsList } from "@/services/apiMentors";
+import {
+  getConsultMentors,
+  getMentoringMentors,
+  getMentorsList,
+  getMockMentors,
+} from "@/services/apiMentors";
 import { useQuery } from "@tanstack/react-query";
 import { Loader } from "lucide-react";
+import { useState } from "react";
 
 const Homepage = () => {
+  const [filter, setFilter] = useState("all");
+
   const { isLoading, data: mentorsList } = useQuery({
-    queryKey: ["mentorsList"],
-    queryFn: getMentorsList,
+    queryKey: ["mentorsList", filter],
+    queryFn: () => {
+      if (filter === "all") {
+        return getMentorsList();
+      } else if (filter === "mock") {
+        return getMockMentors();
+      } else if (filter === "consultation") {
+        return getConsultMentors();
+      } else if (filter === "mentoring") {
+        return getMentoringMentors();
+      } else {
+        return getMentorsList();
+      }
+    },
   });
+
   if (isLoading) return <Loader />;
 
   return (
     <section id="home" className="py-12 flex flex-col items-center">
       <h1 className="text-center font-bold text-xl">Find Your Best Mentor</h1>
       <Search />
-      {/* <Filter /> */}
+
+      <div className="flex gap-2 flex-wrap justify-center">
+        <Button
+          onClick={() => setFilter("all")}
+          className={`bg-muted border-2 border-border text-primary hover:text-white hover:border-primary px-4 md:px-10 
+              ${filter === "all" ? "bg-slate-800 text-white" : " "}`}
+        >
+          All
+        </Button>
+        <Button
+          onClick={() => setFilter("mock")}
+          className={`bg-muted border-2 border-border text-primary hover:text-white hover:border-primary px-4 md:px-10 
+            ${filter === "mock" ? "bg-slate-800 text-white" : " "}`}
+        >
+          Mock interview
+        </Button>
+        <Button
+          onClick={() => setFilter("consultation")}
+          className={`bg-muted border-2 border-border text-primary hover:text-white hover:border-primary px-4 md:px-10 
+            ${filter === "consultation" ? "bg-slate-800 text-white" : " "}`}
+        >
+          Consultation
+        </Button>
+        <Button
+          onClick={() => setFilter("mentoring")}
+          className={`bg-muted border-2 border-border text-primary hover:text-white hover:border-primary px-4 md:px-10 
+            ${filter === "mentoring" ? "bg-slate-800 text-white" : " "}`}
+        >
+          Mentoring
+        </Button>
+      </div>
       <MentorList mentors={mentorsList} />
       <Pagination>
         <PaginationContent>
