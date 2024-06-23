1fbb3aa37f5b4c3bb51368ba7b5d2676e666476a
AhmedAAN <ahmed.a.nagy@outlook.com>
2024-06-23 21:19:25 +0300

    merge error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -36,40 +36,40 @@ const Contact = lazy(() => import("./pages/Contact"));
 const Homepage = lazy(() => import("./pages/Homepage"));
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 60 * 1000,
-    },
-  },
+	defaultOptions: {
+		queries: {
+			staleTime: 60 * 1000,
+		},
+	},
 });
 
 const App = () => {
-  const [isConnected, setIsConnected] = useState(socket.connected);
-  const [messageEvent, setMessageEvent] = useState<any[]>([]);
+	const [isConnected, setIsConnected] = useState(socket.connected);
+	const [messageEvent, setMessageEvent] = useState<any[]>([]);
 
-  useEffect(() => {
-    socket.connect();
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
+	useEffect(() => {
+		socket.connect();
+		return () => {
+			socket.disconnect();
+		};
+	}, []);
 
-  useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
-    }
+	useEffect(() => {
+		function onConnect() {
+			setIsConnected(true);
+		}
 
-    function onDisconnect() {
-      setIsConnected(false);
-    }
+		function onDisconnect() {
+			setIsConnected(false);
+		}
 
-    function onMessageEvent(value: any) {
-      setMessageEvent(value);
-    }
+		function onMessageEvent(value: any) {
+			setMessageEvent(value);
+		}
 
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
-    socket.on("get chats", onMessageEvent);
+		socket.on("connect", onConnect);
+		socket.on("disconnect", onDisconnect);
+		socket.on("get chats", onMessageEvent);
 
 		return () => {
 			socket.off("connect", onConnect);
@@ -77,128 +77,122 @@ const App = () => {
 			socket.off("get chats", onMessageEvent);
 		};
 	}, [messageEvent]);
-    return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-      socket.off("get chats", onMessageEvent);
-    };
-  }, [messageEvent]);
 
-  return (
-    <Suspense fallback={<Loader />}>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <SignupProvider>
-          <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-            <BookingsMentorProvider>
-              <BrowserRouter>
-                <SmallScreenProvider>
-                  <Routes>
-                    <Route element={<Layout />}>
-                      <Route
-                        path="mentor/:id"
-                        element={<Mentor messageEvent={messageEvent} />}
-                      />
-                      <Route index path="/" element={<LandingPage />} />
-                      <Route path="contact" element={<Contact />} />
-                      <Route path="home" element={<Homepage />} />
-                      <Route path="dashboard" element={<Dashboard />} />
-                      <Route path="payment" element={<Payment />} />
-                      <Route
-                        path="studentprofile"
-                        element={<StudentProfile />}
-                      />
-                      <Route
-                        path="testchat"
-                        element={
-                          <TestChat
-                            isConnected={isConnected}
-                            messageEvent={messageEvent}
-                          />
-                        }
-                      />
-                      <Route
-                        path="mentorprofile"
-                        element={
-                          <MentorProfile _id="65dd99b0e731f3477cb5bcb4" />
-                        }
-                      />
+	return (
+		<Suspense fallback={<Loader />}>
+			<QueryClientProvider client={queryClient}>
+				<ReactQueryDevtools initialIsOpen={false} />
+				<SignupProvider>
+					<ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+						<BookingsMentorProvider>
+							<BrowserRouter>
+								<SmallScreenProvider>
+									<Routes>
+										<Route element={<Layout />}>
+											<Route
+												path="mentor/:id"
+												element={<Mentor messageEvent={messageEvent} />}
+											/>
+											<Route index path="/" element={<LandingPage />} />
+											<Route path="contact" element={<Contact />} />
+											<Route path="home" element={<Homepage />} />
+											<Route path="dashboard" element={<Dashboard />} />
+											<Route path="payment" element={<Payment />} />
+											<Route
+												path="studentprofile"
+												element={<StudentProfile />}
+											/>
+											<Route
+												path="testchat"
+												element={
+													<TestChat
+														isConnected={isConnected}
+														messageEvent={messageEvent}
+													/>
+												}
+											/>
+											<Route
+												path="mentorprofile"
+												element={
+													<MentorProfile _id="65dd99b0e731f3477cb5bcb4" />
+												}
+											/>
 
-                      <Route
-                        path="chats"
-                        element={<Chats isConnected={isConnected} />}
-                      />
-                      <Route
-                        path="chats/:id"
-                        element={<Chats isConnected={isConnected} />}
-                      />
-                      <Route path="chatTest" element={<ChatTest />} />
-                      <Route
-                        path="testchat"
-                        element={
-                          <TestChat
-                            isConnected={isConnected}
-                            messageEvent={messageEvent}
-                          />
-                        }
-                      />
-                      <Route
-                        path="mentorprofile"
-                        element={
-                          <MentorProfile _id="65dd99b0e731f3477cb5bcb4" />
-                        }
-                      />
+											<Route
+												path="chats"
+												element={<Chats isConnected={isConnected} />}
+											/>
+											<Route
+												path="chats/:id"
+												element={<Chats isConnected={isConnected} />}
+											/>
+											<Route path="chatTest" element={<ChatTest />} />
+											<Route
+												path="testchat"
+												element={
+													<TestChat
+														isConnected={isConnected}
+														messageEvent={messageEvent}
+													/>
+												}
+											/>
+											<Route
+												path="mentorprofile"
+												element={
+													<MentorProfile _id="65dd99b0e731f3477cb5bcb4" />
+												}
+											/>
 
-                      <Route
-                        path="selectavailable"
-                        element={<SelectTimeAvailable />}
-                      />
-                      <Route
-                        path="studentnotifications"
-                        element={<StudentNotifications />}
-                      />
-                      <Route
-                        path="mentornotifications"
-                        element={<MentorNotifications />}
-                      />
-                      <Route path="selectdate" element={<SelectDate />} />
-                    </Route>
-                    <Route path="login" element={<Login />} />
-                    <Route path="sign-up" element={<SignUp />} />
-                    <Route path="sign-up/mentor" element={<MentorSignup />} />
-                    <Route
-                      path="forget-password"
-                      element={<ForgetPassword />}
-                    />
-                  </Routes>
-                </SmallScreenProvider>
-              </BrowserRouter>
-              <Toaster
-                position="top-center"
-                gutter={12}
-                containerStyle={{ margin: "8px" }}
-                toastOptions={{
-                  success: {
-                    duration: 3000,
-                  },
-                  error: {
-                    duration: 5000,
-                  },
-                  style: {
-                    fontSize: "16px",
-                    maxWidth: "500px",
-                    padding: "16px 24px",
-                    backgroundColor: "white",
-                    color: "black",
-                  },
-                }}
-              />
-            </BookingsMentorProvider>
-          </ThemeProvider>
-        </SignupProvider>
-      </QueryClientProvider>
-    </Suspense>
-  );
+											<Route
+												path="selectavailable"
+												element={<SelectTimeAvailable />}
+											/>
+											<Route
+												path="studentnotifications"
+												element={<StudentNotifications />}
+											/>
+											<Route
+												path="mentornotifications"
+												element={<MentorNotifications />}
+											/>
+											<Route path="selectdate" element={<SelectDate />} />
+										</Route>
+										<Route path="login" element={<Login />} />
+										<Route path="sign-up" element={<SignUp />} />
+										<Route path="sign-up/mentor" element={<MentorSignup />} />
+										<Route
+											path="forget-password"
+											element={<ForgetPassword />}
+										/>
+									</Routes>
+								</SmallScreenProvider>
+							</BrowserRouter>
+							<Toaster
+								position="top-center"
+								gutter={12}
+								containerStyle={{ margin: "8px" }}
+								toastOptions={{
+									success: {
+										duration: 3000,
+									},
+									error: {
+										duration: 5000,
+									},
+									style: {
+										fontSize: "16px",
+										maxWidth: "500px",
+										padding: "16px 24px",
+										backgroundColor: "white",
+										color: "black",
+									},
+								}}
+							/>
+						</BookingsMentorProvider>
+					</ThemeProvider>
+				</SignupProvider>
+			</QueryClientProvider>
+		</Suspense>
+	);
 };
 
 export default App;
